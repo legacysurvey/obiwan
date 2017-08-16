2c21084247a45f52f18749f71eedb351bb355230
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-08-16 13:45:02 -0700

    added main

--- py/obiwan/runs.py
+++ py/obiwan/runs.py
@@ -19,23 +19,24 @@ def in_eboss(T):
     z= np.logical_and.reduce((T.ra > 126., T.ra < 169.,T.dec > 14.,T.dec < 29.))
     return np.logical_or.reduce((x,y,z)) 
 
-eboss_fns= glob(os.path.join(DOWNLOAD_ROOT,'configs/dr3eBOSS/additional_ccds',
+if __name__ == "__main__":
+  eboss_fns= glob(os.path.join(NERSC_ROOT,'configs/dr3eBOSS/additional_ccds',
+                               "survey-ccds-*.fits.gz"))
+  dr3_fns= glob(os.path.join(NERSC_ROOT,'configs/dr3eBOSS/dr3',
                              "survey-ccds-*.fits.gz"))
-dr3_fns= glob(os.path.join(DOWNLOAD_ROOT,'configs/dr3eBOSS/dr3',
-                           "survey-ccds-*.fits.gz"))
 
 
-dr3= CatalogueFuncs().stack(dr3_fns,textfile=False)
-eboss= CatalogueFuncs().stack(eboss_fns,textfile=False)
+  dr3= CatalogueFuncs().stack(dr3_fns,textfile=False)
+  eboss= CatalogueFuncs().stack(eboss_fns,textfile=False)
 
-dr3.set('pid', np.char.add(dr3.expnum.astype(str),
-                           np.char.strip(dr3.image_filename)))
-eboss.set('pid', np.char.add(eboss.expnum.astype(str),
-                             np.char.strip(eboss.image_filename))
+  dr3.set('pid', np.char.add(dr3.expnum.astype(str),
+                             np.char.strip(dr3.image_filename)))
+  eboss.set('pid', np.char.add(eboss.expnum.astype(str),
+                               np.char.strip(eboss.image_filename))
 
-dr3.cut( in_eboss(dr3))
-eboss.cut( in_eboss(eboss))
+  dr3.cut( in_eboss(dr3))
+  eboss.cut( in_eboss(eboss))
 
-len( set(dr3.pid).difference(set(eboss.pid)) )
-len( set(eboss.pid).difference(set(dr3.pid)) )
-len( set(eboss.pid).intersection(set(dr3.pid)) )
+  len( set(dr3.pid).difference(set(eboss.pid)) )
+  len( set(eboss.pid).difference(set(dr3.pid)) )
+  len( set(eboss.pid).intersection(set(dr3.pid)) )
