f3e173392f6cfc884c8d09d8998f0956a24b8fb9
Kaylan Burleigh <kburleigh@lbl.gov>
2017-11-25 18:59:26 -0800

    updated main scripts for MoG changes

--- bin/slurm_job_randoms.sh
+++ bin/slurm_job_randoms.sh
@@ -10,12 +10,11 @@
 
 source $CSCRATCH/obiwan_code/obiwan/bin/run_atnersc/bashrc_obiwan
 export name_for_run=elg_100deg2
-export ra1=173.5
-export ra2=176.5
-export dec1=23.0
-export dec2=26.0
-export nrandoms=240000
-export kdedir=$CSCRATCH/obiwan_out/randoms
+export ra1=150.0
+export ra2=160.0
+export dec1=0.
+export dec2=10.0
+export nrandoms=2400000
 export outdir=$CSCRATCH/obiwan_out/randoms/${name_for_run}
 
 # NERSC / Cray / Cori / Cori KNL things
@@ -26,6 +25,6 @@ export OMP_NUM_THREADS=1
 
 srun -n 32 -c 1 \
     python $obiwan_code/obiwan/py/obiwan/draw_radec_color_z.py \
-    --dowhat sample --obj elg --ra1 ${ra1} --ra2 ${ra2} --dec1 ${dec1} --dec2 ${dec2} \
-    --ndraws ${nrandoms} --kdedir ${kdedir} --outdir ${outdir}
+    --obj elg --ra1 ${ra1} --ra2 ${ra2} --dec1 ${dec1} --dec2 ${dec2} \
+    --ndraws ${nrandoms} --outdir ${outdir} --nproc 32
 
