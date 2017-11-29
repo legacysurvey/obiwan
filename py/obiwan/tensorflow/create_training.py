01077d3b76b26e0e3d2a0baaaf0e093d358137d7
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-11-28 18:55:52 -0800

    updated for new dir structure

--- py/obiwan/tensorflow/create_training.py
+++ py/obiwan/tensorflow/create_training.py
@@ -8,117 +8,59 @@ from astrometry.util.fits import fits_table
 
 from legacypipe.survey import LegacySurveyData, wcs_for_brick
 from obiwan.qa.visual import readImage,sliceImage
-from obiwan.common import dobash
+from obiwan.common import dobash,get_rsdir
+from obiwan.runmanager.status import get_final_dir
 
 import galsim
 
 HDF5_KEYS= ['g','r','z','gr','gz','rz','grz']
 
-class BrickStamps(object):
-    """Object for exracting all sims and real galaxy cutouts 
+def flux2mag(nmgy):
+    return -2.5 * (np.log10(nmgy) - 9)
+
+class SimStamps(object):
+    """Object for exracting sim cutouts 
 
         Args:
             ls_dir: LEGACY_SURVEY_DIR, like 'tests/end_to_end/testcase_DR5_grz'
-            obj_dir: path to obj/ dir for obiwan or brick dir for tractor, e.g.
-                '$HOME/myrepo/obiwan/tests/end_to_end/out_testcase_DR5_grz_allblobs'
-
+            outdir: path to dir containing obiwan,coadd,tractor dirs
     """
 
-    def __init__(self,ls_dir=None,obj_dir=None):
-        self.obj_dir= obj_dir
+    def __init__(self,outdir=None,
+                 ls_dir=None):
+        """outdir: required
+           ls_dir: not needed if env var LEGACY_SURVEY_DIR already set
+        """
+        self.outdir= outdir
         if ls_dir:
             os.environ["LEGACY_SURVEY_DIR"]= ls_dir
         self.survey = LegacySurveyData()
 
-    def run(self,brick,rs_dirs=[],isTractor=True,
-            savedir=None, zoom=None):
-        """Write the hdf5 image files for all rs/* in this brick
+    def get_brickwcs(self,brick):
+        brickinfo = self.survey.get_brick_by_name(brick)
+        self.brickwcs = wcs_for_brick(brickinfo)
 
-        Args:
-            brick:
-            rs_dirs: list of dirs to the coadd/,tractor/ dirs for this brick
-                For obiwan, this is many rs*/ dirs, for DR5 this is a single dir
-            isTractor: boolean, is the catalouge a Tractor cat or a simcat cat?
-            savedir: where to write the hdf5 cutouts 
-            zoom: if legacypipe was run with zoom option
-        """
-        self.get_brickwcs(brick)
-        assert(len(rs_dirs) > 0)
-        # coadd fits images must exist
-        coadd_fns= glob(os.path.join(rs_dirs[0],'coadd',
-                                     brick[:3],brick,'*-image-*.fits.fz'))
-        print('rs_dirs[0]=',rs_dirs[0])
-        assert(len(coadd_fns) > 0)
-        # set of bands in this brick
-        self.bands= (pd.Series(coadd_fns)
-                     .str.replace('.fits.fz','')
-                     .str[-1].values)
-        assert(self.bands.size > 0)
-        # One hdf5 file for this brick
-        # like '_rz.hdf5'
-        self.bands_str= ''.join(sorted(self.bands))
-        assert(self.bands_str in HDF5_KEYS)
-        hdf5_fn= os.path.join(savedir,
-                              'img_ivar_%s.hdf5' % self.bands_str)
-        hdf5_fn_onedge= hdf5_fn.replace('.hdf5','_onedge.hdf5')
-        if os.path.exists(hdf5_fn):
-            print('Skipping %s, hdf5 already exists: %s' % (brick,hdf5_fn))
-            return None
-        self.hdf5_obj = h5py.File(hdf5_fn, "w")
-        self.hdf5_obj_onedge = h5py.File(hdf5_fn_onedge, "w")
-        # Many rs*/ dirs per brick
-        for rs_dir in rs_dirs:
-            self.load_brick(brick=brick,rs_dir=rs_dir,isTractor=isTractor)
-            self.add_xyid(zoom=zoom)
-            self.apply_cuts()
-            self.extract()
-        self.hdf5_obj.close()
-        self.hdf5_obj_onedge.close()
-        print('Wrote %s' % hdf5_fn)
-        print('Wrote %s'% hdf5_fn_onedge)
-        
-    def load_brick(self,brick,rs_dir,isTractor=True):
-        """loads all necessary info for each brick,rs_dir combination
+    def load_data(self,brick,cat_fn,coadd_dir):
+        """loads coadd and catalogue data
 
         Args:
             brick:
             rs_dir: path/to/rs0, rs300, rs300_skipid, etc
-            cat_fn: name of catalogue relative to the rs_dirs, e.g. for simcat its
-                'obiwan/simcat-elg-%s.fits' % brick
         """
-        print('Loading from %s' % rs_dir)
-        if isTractor:
-            self.cat= fits_table(os.path.join(rs_dir,'tractor',
-                                 brick[:3],'tractor-%s.fits' % brick))
-        else:
-            self.cat= fits_table(os.path.join(rs_dir,'obiwan',
-                                              brick[:3],brick,'simcat.fits'))
+        print('Loading from %s' % coadd_dir)
+        self.cat= fits_table(cat_fn)
 
         self.img_fits,self.ivar_fits= {},{}
         for b in self.bands: 
-            self.img_fits[b]= readImage(os.path.join(rs_dir,'coadd',brick[:3],brick,
+            self.img_fits[b]= readImage(coadd_dir,
                                         'legacysurvey-%s-image-%s.fits.fz' % (brick,b)))
-            self.ivar_fits[b]= readImage(os.path.join(rs_dir,'coadd',brick[:3],brick,
+            self.ivar_fits[b]= readImage(coadd_dir,
                                         'legacysurvey-%s-invvar-%s.fits.fz' % (brick,b)))
         # galsim.Image() so can determine overlap w/cutouts
         self.img_gs,self.ivar_gs= {},{}
         for b in self.bands:
             self.img_gs[b]= galsim.Image(self.img_fits[b])
             self.ivar_gs[b]= galsim.Image(self.ivar_fits[b])
-
-    def get_brickwcs(self,brick):
-        brickinfo = self.survey.get_brick_by_name(brick)
-        self.brickwcs = wcs_for_brick(brickinfo)
-
-    def set_xyid(self,zoom=None):
-        # Require x,y,id columns in self.simcat and x,y are ints
-        for col in ['x','y','id']:
-            assert(col in self.cat.get_columns())
-
-    def apply_cuts(self):
-        print('len(cat) b4 cut: %d' % len(self.cat))
-        self.cat.cut(np.ones(len(self.cat),bool))
-        print('len(cat) after cut: %d' % len(self.cat))
 
     def extract(self,hw=20):
         """For each id,x,y in self.cat, extracts image cutout
@@ -173,57 +115,130 @@ class BrickStamps(object):
                                                         chunks=True, \
                     data= np.array([d.array for d in ivar]).T)
 
+    def run(self,brick,zoom=None):
+        """Write the hdf5 image files for all rs/* in this brick
 
-class SimcatStamps(BrickStamps):
-    def __init__(self,ls_dir=None,obj_dir=None):
-        super(SimcatStamps,self).__init__(ls_dir=ls_dir,
-                                          obj_dir=obj_dir)
+        Args:
+            brick: brickname
+            zoom: if legacypipe was run with zoom option
+        """
+        self.get_brickwcs(brick)
+        assert(len(rs_dirs) > 0)
+        # coadd fits images must exist
+        self.set_paths_to_data(brick)
+        coadd_fns= glob(os.path.join(self.coadd_dirs[0],
+                                     '*-image-*.fits.fz'))
+        assert(len(coadd_fns) > 0)
+        # set of bands in this brick
+        self.bands= (pd.Series(coadd_fns)
+                     .str.replace('.fits.fz','')
+                     .str[-1].values)
+        assert(self.bands.size > 0)
+        # One hdf5 file for this brick, like '_rz.hdf5'
+        self.bands_str= ''.join(sorted(self.bands))
+        assert(self.bands_str in HDF5_KEYS)
+        self.set_hdf5_fns(brick,self.bands_str)
+    
+        if os.path.exists(self.hdf5_fn):
+            print('Skipping %s, hdf5 already exists: %s' % (brick,self.hdf5_fn))
+            return None
+        self.hdf5_obj = h5py.File(self.hdf5_fn, "w")
+        self.hdf5_obj_onedge = h5py.File(self.hdf5_fn_onedge, "w")
+        # Many rs*/ dirs per brick
+        for cat_fn,coadd_dir in zip(self.cat_fns,self.coadd_dirs):
+            self.load_data(brick,cat_fn,coadd_dir)
+            self.add_xyid(zoom=zoom)
+            self.apply_cuts()
+            self.extract()
+        self.hdf5_obj.close()
+        self.hdf5_obj_onedge.close()
+        print('Wrote %s' % hdf5_fn)
+        print('Wrote %s'% hdf5_fn_onedge)
+        
+    def set_paths_to_data(self,brick):
+        """lists of catalogues filenames and coadd dirs"""
+        rs_dirs= glob(os.path.join(self.outdir,'coadd',
+                                   brick[:3],brick,'*rs*'))
+        rs_dirs= [os.path.basename(a)
+                  for a in rs_dirs]
+        for rs_dir in rs_dirs:
+            self.cat_fns= os.path.join(self.outdir,'obiwan',
+                                       brick[:3],brick,rs_dir,
+                                       'simcat.fits')
+            self.coadd_dirs= os.path.join(self.outdir,'coadd',
+                                       brick[:3],brick,rs_dir)
 
-    def run(self,brick,savedir=None,zoom=None):
-        d=dict(rs_dirs=glob(os.path.join(self.obj_dir,'elg/%s/%s/*rs*' % \
-                                   (brick[:3],brick))),
-               isTractor=False,
-               savedir=savedir,
-               zoom=zoom,
-               )
-        super(SimcatStamps,self).run(brick,**d)
+    def set_hdf5_fns(self,brick,bands_str):
+        dr= os.path.join(self.outdir,'hdf5',
+                         brick[:3],brick)
+        self.hdf5_fn= os.path.join(dr,
+                                   'img_ivar_%s.hdf5' % bands_str)
+        self.hdf5_fn_onedge= self.hdf5_fn.replace('.hdf5',
+                                        '_onedge.hdf5')
+        try:
+            dobash('mkdir -p %s' % dr)
+        except ValueError:
+            print('hdf5 dir already exists: ',dr)
 
-    def add_xyid(self,zoom=None):
+    def set_xyid(self,zoom=None):
         _,x,y=self.brickwcs.radec2pixelxy(self.cat.ra,self.cat.dec)
         if zoom:
             x -= zoom[0]
             y -= zoom[2]
         self.cat.set('x',x.astype(int))
         self.cat.set('y',y.astype(int))
+        assert('id' in self.cat.get_columns())
 
-def flux2mag(nmgy):
-    return -2.5 * (np.log10(nmgy) - 9)
+    def apply_cuts(self):
+        pass
 
-class TractorStamps(BrickStamps):
-    def __init__(self,ls_dir=None,obj_dir=None):
+        
+
+class TractorStamps(SimStamps):
+    def __init__(self,ls_dir=None,outdir=None,
+                 savedir=None):
+        """Same as SimStamps but for tractor catalogues
+
+        Args:
+            savedir: required for tractor not sims b/c cannot write to dr5 dir
+        """
         super(TractorStamps,self).__init__(ls_dir=ls_dir,
-                                           obj_dir=obj_dir)
+                                           outdir=outdir)
+        self.savedir= savedir
+    
+    def set_paths_to_data(self,brick):
+        """lists of catalogues filenames and coadd dirs"""
+        rs_dirs= 
+        for rs_dir in rs_dirs:
+            self.cat_fns= [os.path.join(self.outdir,'tractor',
+                                        brick[:3],
+                                        'tractor-%s.fits' % brick)]
+            self.coadd_dirs= [os.path.join(self.outdir,'coadd',
+                                           brick[:3],brick)]
 
-    def run(self,brick,savedir=None,zoom=None):
-        d=dict(rs_dirs= [self.obj_dir],
-               isTractor=True,
-               savedir=savedir,
-               zoom=zoom,
-               )
-        super(TractorStamps,self).run(brick,**d)
+    def set_hdf5_fns(self,brick,bands_str):
+        dr= os.path.join(self.savedir,'hdf5',
+                         brick[:3],brick)
+        self.hdf5_fn= os.path.join(dr,
+                                   'img_ivar_%s.hdf5' % bands_str)
+        self.hdf5_fn_onedge= self.hdf5_fn.replace('.hdf5',
+                                        '_onedge.hdf5')
+        try:
+            dobash('mkdir -p %s' % dr)
+        except ValueError:
+            print('hdf5 dir already exists: ',dr)
 
-    def add_xyid(self,zoom=None):
+    def set_xyid(self,zoom=None):
         x,y=self.cat.bx,self.cat.by
         if zoom:
             x -= zoom[0]
             y -= zoom[2]
         self.cat.set('x',x.astype(int))
         self.cat.set('y',y.astype(int))
-        # ID
         self.cat.set('id',self.cat.objid)
 
     def apply_cuts(self):
-        print('len(cat) b4 cut: %d' % len(self.cat))
+        len_bef= len(self.cat)
         self.cat.cut((self.cat.brick_primary) &
                      (self.cat.nobs_g >= 1) &
                      (self.cat.nobs_r >= 1) &
@@ -231,11 +246,10 @@ class TractorStamps(BrickStamps):
                      (self.cat.allmask_g == 0) & 
                      (self.cat.allmask_r == 0) & 
                      (self.cat.allmask_z == 0)) 
-        print('len(cat) after cut: %d' % len(self.cat))
         self.cat.cut((flux2mag(self.cat.flux_g) > 20.) &
                      (flux2mag(self.cat.flux_r) > 20.) &
                      (flux2mag(self.cat.flux_z) > 19.))
-        print('len(cat) after cut: %d' % len(self.cat))
+        print('After cut, have %d/%d' % (len(self.cat),len_bef))
 
 
 def testcase_main(): 
@@ -261,35 +275,48 @@ def testcase_main():
     for brick in [brick]:
         Sim.run(brick, zoom=zoom)
 
-def mpi_main():
-    #from mpi4py.MPI import COMM_WORLD as comm
-    #bricks= np.loadtxt('bricks_all.txt',dtype=str)
-    tractor=True
-    if not tractor:
-        obj_dir= '/global/cscratch1/sd/kaylanb/obiwan_out/elg_100deg2'
-    else:
-        obj_dir= '/global/project/projectdirs/cosmo/data/legacysurvey/dr5'
-        my_dir= '/global/cscratch1/sd/kaylanb/obiwan_out/elg_100deg2'
+def mpi_main(nproc=1,which=None,
+             outdir=None,ls_dir=None,savedir=None,
+             bricks=[]):
+    """
 
-    #rank_bricks= np.array_split(bricks, comm.size)[comm.rank]
-    #Sim= SimcatStamps(ls_dir=None, obj_dir=obj_dir)
-    Trac= TractorStamps(ls_dir=None, obj_dir=obj_dir)
-    #for brick in rank_bricks:
-    for brick in ['3381m007']:
-        #print('rank %d working on brick %s' % (comm.rank,brick))
-        if not tractor:
-            savedir= os.path.join(obj_dir,'elg',brick[:3],brick)
-        else:
-            savedir= os.path.join(my_dir,'hdf5',brick[:3],brick)
-        try:
-            dobash('mkdir -p %s' % savedir)
-        except ValueError:
-            print('dir already exists %s' % savedir)
-        #Sim.run(brick,savedir=savedir)
-        Trac.run(brick,savedir=savedir)
+    Args:
+        nproc: > 1 for mpi4py
+        which: one of ['tractor','sim']
+        outdir: path to coadd,tractor dirs
+        ls_dir: not needed if legacy_survey_dir env var already set
+        savedir: only needed if which = tractor, where to write the hdf5 files
+        bricks: list bricks to make hdf5 cutouts from
+    """
+    assert(which in ['tractor','sim'])
+    if nproc > 1:
+        from mpi4py.MPI import COMM_WORLD as comm
+        bricks= np.array_split(bricks, comm.size)[comm.rank]
+
+    if which == 'sim':
+        Obj= SimStamps(outdir=outdir,ls_dir=ls_dir)
+    else:
+        Obj= TractorStamps(outdir=outdir,ls_dir=ls_dir,
+                           savedir=savedir)
+    
+    for brick in bricks:
+        Obj.run(brick)
 
 
 if __name__ == '__main__':
     #testcase_main()
-    mpi_main()
+
+    
+
+    mpi_main(nproc=1,which='sim',
+             outdir='/global/cscratch1/sd/kaylanb/obiwan_out/elg_dr5_coadds',
+             savedir='',
+             ls_dir=None,
+             bricks=bricks)
+
+    mpi_main(nproc=1,which='tractor',
+             outdir='/global/project/projectdirs/cosmo/data/legacysurvey/dr5',
+             savedir='/global/cscratch1/sd/kaylanb/obiwan_out/dr5_cutouts',
+             ls_dir=None,
+             bricks=bricks)
 
