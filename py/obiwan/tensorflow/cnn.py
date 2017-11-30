7ec9b39bccbee458321e6330cc9f610f6c25b0c2
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-11-30 09:27:25 -0800

    added cnn from ipynb and mpi script to split all hdf5 into train/test numpy binaries

---
+++ py/obiwan/tensorflow/cnn.py
@@ -0,0 +1,112 @@
+"""
+Credit: https://github.com/ageron/handson-ml
+Adapted from their CNN ipynb in chapter 13
+"""
+
+import numpy as np
+import os
+import tensorflow as tf
+
+height,width,channels = (64,64,6) 
+
+conv_kwargs= dict(strides=1,
+                  padding='SAME',
+                  activation=tf.nn.relu)
+pool_kwargs= dict(ksize= [1,2,2,1],
+                  strides=[1,2,2,1],
+                  padding='VALID')
+
+with tf.name_scope("inputs"):
+    X = tf.placeholder(tf.float32, shape=[None,height,width,channels], name="X") #training data shape
+    #X_reshaped = tf.reshape(X, shape=[-1, height, width, channels])
+    y = tf.placeholder(tf.int32, shape=[None], name="y") 
+
+
+
+# 64x64
+with tf.name_scope("layer1"):
+    conv1 = tf.layers.conv2d(X, filters=3*channels, kernel_size=7,
+                             **conv_kwargs)
+    pool1 = tf.nn.avg_pool(conv1, **pool_kwargs)
+
+# 32x32
+with tf.name_scope("layer2"):
+    conv2 = tf.layers.conv2d(pool1, filters=6*channels, kernel_size=7,
+                             **conv_kwargs)
+    pool2 = tf.nn.avg_pool(conv2, **pool_kwargs)
+
+# 16x16
+with tf.name_scope("layer3"):
+    conv3 = tf.layers.conv2d(pool2, filters=9*channels, kernel_size=7,
+                             **conv_kwargs)
+    pool3 = tf.nn.avg_pool(conv3, **pool_kwargs)
+    # next is fc
+    pool3_flat = tf.reshape(pool3, 
+                            shape=[-1, pool3.shape[1] * pool3.shape[2] * pool3.shape[3]])
+
+
+with tf.name_scope("fc"):
+    fc = tf.layers.dense(pool3_flat, 64, activation=tf.nn.relu, name="fc")
+
+with tf.name_scope("output"):
+    logits = tf.layers.dense(fc, 2, name="output") # 2 classes
+    Y_proba = tf.nn.softmax(logits, name="Y_proba")
+
+with tf.name_scope("train"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+    loss = tf.reduce_mean(xentropy)
+    optimizer = tf.train.AdamOptimizer()
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+with tf.name_scope("init_and_save"):
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+
+# Sanity check
+def get_data_dir(brick):
+    if os.environ['HOME'] == '/Users/kaylan1':
+        dr= os.path.join(os.environ['HOME'],'Downloads')
+    else: 
+        dr= os.path.join(os.environ['CSCRATCH'],'obiwan_out')
+    return os.path.join(dr,'dr5_testtrain','testtrain',brick[:3],brick) 
+    
+dr= get_data_dir('1211p060')
+Xtrain= np.load(os.path.join(dr,'xtrain_1.npy'))
+Ytrain= np.load(os.path.join(dr,'ytrain_1.npy'))
+
+batch_size=32
+X_batch,y_batch= Xtrain[:batch_size,...],Ytrain[:batch_size]
+
+with tf.Session() as sess:
+    init.run()
+    print(sess.run(X, feed_dict={X: X_batch}).shape)
+    print(sess.run(conv1, feed_dict={X: X_batch}).shape)
+    print(sess.run(pool1, feed_dict={X: X_batch}).shape)
+    print(sess.run(conv2, feed_dict={X: X_batch}).shape)
+    print(sess.run(pool2, feed_dict={X: X_batch}).shape)
+    print(sess.run(conv3, feed_dict={X: X_batch}).shape)
+    print(sess.run(pool3, feed_dict={X: X_batch}).shape)
+    print(sess.run(pool3_flat, feed_dict={X: X_batch}).shape)
+    print(sess.run(fc, feed_dict={X: X_batch}).shape)
+    print(sess.run(logits, feed_dict={X: X_batch}).shape) 
+
+# Train
+n_epochs = 8
+batch_size = 32
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for cnt in range(Xtrain.shape[0] // batch_size):
+            slc= slice(cnt*batch_size,(cnt+1)*batch_size)
+            X_batch, y_batch= Xtrain[slc,...],Ytrain[slc]
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        print(epoch, "Train accuracy:", acc_train)
+
+        save_path = saver.save(sess, "./cnn_model")
---
+++ py/obiwan/tensorflow/split_testtrain.py
@@ -0,0 +1,116 @@
+import numpy as np
+import os
+from glob import glob
+import h5py
+
+def get_data(f,keys):
+    """Returns numpy array of shape [len(keys),64,64,6]
+    
+    Args:
+        f: hdf5 file object
+    """
+    return np.array([np.stack([f[key+'/img'],f[key+'/ivar']],axis=-1).reshape((64,64,6))
+                     for key in keys])
+
+def write_traintest(brick,real_dir,sim_dir,save_dir,
+                    n_train=256,n_test=64):
+    """Writes xtrain_1.npy,xtest_1.npy,... for a given brick
+
+    Args:
+        brick: brickname
+        real_dir: path to hdr5 dir for real galaxies
+        sim_dir: ... simulated galaxies
+        save_dir: where to write the bri/brick/xtrain.npy, ..., files
+    """
+    bri=brick[:3]
+    f_real= h5py.File(os.path.join(real_dir,'hdf5',bri,brick,
+                                   'img_ivar_grz.hdf5'),
+                      'r')
+    f_sim= h5py.File(os.path.join(sim_dir,'hdf5',bri,brick,
+                                   'img_ivar_grz.hdf5'),
+                      'r')
+    keys_real= np.array(list(f_real.keys()))
+    keys_sim= np.array(list(f_sim.keys()))
+    n_samples= np.min([keys_real.size,keys_sim.size])
+    print('n_samples=',n_samples)
+
+    # Shuffle real
+    ind=np.arange(keys_real.size).astype(int)
+    np.random.shuffle(ind)
+    keys_real= keys_real[ind]
+
+    # Sort sim by id is equiv to shuffling
+    keys_sim= np.sort(keys_sim.astype(np.int32)).astype(str)
+
+    # Take equal size samples
+    keys_real= keys_real[:n_samples]
+    keys_sim= keys_sim[:n_samples]
+
+    chunk_size= n_train + n_test
+    print('chunk_size=',chunk_size)
+    # chunks of equal size exept last chunk is whateve is left over
+    #ind= np.array_split(np.arange(n_samples),n_samples // chunk_size + 1)
+    #print('ind.shape=',ind[0].shape)
+    for cnt in range(n_samples // chunk_size + 1): # in enumerate(ind):
+        slc= slice(cnt*chunk_size,(cnt+1)*chunk_size)
+        print('slice= ',slc)
+        print('Brick=%s, chunk=%d' % (brick,cnt+1)) 
+        # Shape [chunk_size,64,64,6]
+        Xreal= get_data(f_real,keys_real[slc])
+        Yreal= np.zeros(len(keys_real[slc]))
+        Xsim= get_data(f_sim,keys_sim[slc])
+        Ysim= np.ones(len(keys_sim[slc]))
+        print(Xreal.shape,Xsim.shape,Yreal.shape,Ysim.shape)
+        # Combine and shuffle
+        x= np.vstack([Xreal,Xsim])
+        y= np.hstack([Yreal,Ysim])
+        print(x.shape,y.shape)
+        shuff= np.arange(x.shape[0])
+        np.random.shuffle(shuff)
+        x= x[shuff,...]
+        y= y[shuff]
+        # Split
+        isplit= 2*n_train
+        if x.shape[0] < 2*chunk_size:
+            isplit= int(float(n_train/chunk_size) * x.shape[0])
+        print('isplit=',isplit)
+        Xtrain,Xtest= x[:isplit,...],x[isplit:,...]
+        Ytrain,Ytest= y[:isplit],y[isplit:]
+        print(Xtrain.shape,Xtest.shape,Ytrain.shape,Ytest.shape)
+        # Save
+        dr= os.path.join(save_dir,'testtrain',bri,brick)
+        try: 
+            os.makedirs(dr)
+        except OSError:
+            print('directory already exists: ',dr)
+        for data,name in zip([Xtrain,Xtest,Ytrain,Ytest],
+                             ['xtrain','xtest','ytrain','ytest']):
+            fn=os.path.join(dr,'%s_%d.npy' % (name,cnt+1))
+            np.save(fn,data)
+            print('Wrote %s' % fn)
+
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--nproc', type=int, default=1, help='set to > 1 to run mpi4py') 
+    parser.add_argument('--bricks_fn', type=str, default=None, help='specify a fn listing bricks to run, or a single default brick will be ran') 
+    parser.add_argument('--real_dir', type=str, required=True, help='path to hdr5 dir for real galaxies') 
+    parser.add_argument('--sim_dir', type=str, required=True, help='ditto for sim galaxies') 
+    parser.add_argument('--save_dir', type=str, required=True, help='where to write the bri/brick/xtrain.npy, ..., files') 
+    args = parser.parse_args()
+
+    if args.bricks_fn:
+        bricks= np.loadtxt(args.bricks_fn,dtype=str)
+    else:
+        bricks=['1211p060']
+
+    if args.nproc > 1:
+        from mpi4py.MPI import COMM_WORLD as comm
+        bricks= np.array_split(bricks, comm.size)[comm.rank]
+
+    for brick in bricks:
+        write_traintest(brick,args.real_dir,args.sim_dir,args.save_dir)
+
+
