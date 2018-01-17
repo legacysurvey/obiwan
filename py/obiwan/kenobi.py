e8ad7d462836841d660b39c3de8d2709523cce42
Kaylan Burleigh <kaylanb@berkeley.edu>
2018-01-17 09:58:15 -0800

    added dataset: cosmos subsets

--- py/obiwan/kenobi.py
+++ py/obiwan/kenobi.py
@@ -43,6 +43,7 @@ try:
     from legacypipe.runbrick import run_brick
     from legacypipe.decam import DecamImage
     from legacypipe.survey import LegacySurveyData, wcs_for_brick
+    from legacypipe.runcosmos import DecamImagePlusNoise, CosmosSurvey 
 
     from astrometry.util.fits import fits_table, merge_tables
     from astrometry.util.ttime import Time
@@ -152,9 +153,15 @@ try:
         
         def __init__(self, dataset=None, survey_dir=None, metacat=None, simcat=None, 
                      output_dir=None,add_sim_noise=False, seed=0,
-                     image_eq_model=False):
-            super(SimDecals, self).__init__(survey_dir=survey_dir, output_dir=output_dir)
+                     image_eq_model=False,subset=None):
             self.dataset= dataset
+            
+            kw= dict(survey_dir=survey_dir, 
+                     output_dir=output_dir)
+            if self.dataset == 'cosmos':
+                kw.update(subset=subset)
+            super(SimDecals, self).__init__(**kw)
+
             self.metacat = metacat
             self.simcat = simcat
             # Additional options from command line
@@ -164,7 +171,10 @@ try:
             print('SimDecals: self.image_eq_model=',self.image_eq_model)
             
         def get_image_object(self, t):
-            return SimImage(self, t)
+            if self.dataset == 'cosmos':
+                return SimImageCosmos(self, t)
+            else:
+                return SimImage(self, t)
         
         def filter_ccds_files(self, fns):
             """see legacypipe/runs.py"""
@@ -215,6 +225,22 @@ try:
         sat= saturation_e(camera) / nano2e**2
         var[flag]= sat
         return var 
+except NameError:
+    pass
+
+try: 
+    class SimDecalsCosmos(SimDecals,CosmosSurvey):
+        """Filters the CCDs to just those in the cosmos realizations
+
+        Call just like SimDecals except with additional Argument 'subset'        
+        
+        Args:
+            **kwargs: SimDecals args + 'subset'
+        """
+        
+        def __init__(self, survey_dir, output_dir, subset):
+            super(SimDecalsCosmos, self).__init__(survey_dir=survey_dir, output_dir=output_dir,
+                                                  subset=subset)
 except NameError:
     pass
 
@@ -339,6 +365,21 @@ try:
                 tim.data = tim_image.array
                 tim.inverr = np.sqrt(tim_invvar.array)
             return tim
+except NameError:
+    pass
+
+try: 
+    class SimImageCosmos(SimImage,DecamImagePlusNoise):
+        """Filters the CCDs to just those in the cosmos realizations
+
+        Call just like SimDecals except with additional Argument 'subset'        
+        
+        Args:
+            **kwargs: SimDecals args + 'subset'
+        """
+        
+        def __init__(self, survey, t):
+            super(SimImageCosmos, self).__init__(survey, t)
 except NameError:
     pass
 
@@ -628,6 +669,8 @@ def get_parser():
                         help='if using mpi4py')
     parser.add_argument('--all_blobs', action='store_true',default=False,
                         help='fit models to all blobs, not just those containing sim sources')
+    parser.add_argument('--subset', type=int, default=0,
+                        help='COSMOS subset number [0 to 4, 10 to 12], only used if dataset = cosmos')
     parser.add_argument('--checkpoint', action='store_true',default=False,
                         help='turn on checkpointing')
     parser.add_argument('--overwrite_if_exists', action='store_true',default=False,
@@ -809,10 +852,16 @@ def do_one_chunk(d=None):
         Nothing, but this func end ups writing out all the obiwan results 
     """
     assert(d is not None)
-    simdecals = SimDecals(dataset=d['args'].dataset,\
-              metacat=d['metacat'], simcat=d['simcat'], output_dir=d['simcat_dir'], \
-              add_sim_noise=d['args'].add_sim_noise, seed=d['seed'],\
-              image_eq_model=d['args'].image_eq_model)
+    kw= dict(dataset=d['args'].dataset,\
+             metacat=d['metacat'], simcat=d['simcat'], \
+             output_dir=d['simcat_dir'], \
+             add_sim_noise=d['args'].add_sim_noise, seed=d['seed'],\
+             image_eq_model=d['args'].image_eq_model)
+    if d['args'].dataset == 'cosmos':
+        kw.update(subset=d['args'].subset)
+        simdecals= SimDecalsCosmos(**kw)
+    else:
+        simdecals = SimDecals(**kw)
     # Use Tractor to just process the blobs containing the simulated sources.
     if d['args'].all_blobs:
         blobxy = None
