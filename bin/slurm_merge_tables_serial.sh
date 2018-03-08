d05d8e90ad1d19d8b07a9111ad314ba4573253af
Kaylan Burleigh <kburleigh@lbl.gov>
2018-03-08 11:53:26 -0800

    updated merging to avoid mem error

--- bin/slurm_merge_tables_serial.sh
+++ bin/slurm_merge_tables_serial.sh
@@ -2,20 +2,21 @@
 
 #SBATCH -p debug
 #SBATCH -N 1
-#SBATCH -t 00:10:00
+#SBATCH -t 00:20:00
 #SBATCH --account=desi
 #SBATCH -J randoms
 #SBATCH -L SCRATCH,project
 ###SBATCH -C haswell
 
-#export doWhat=randoms
-export doWhat=summary
-export outdir=eboss_elg
+export doWhat=randoms
+#export doWhat=summary
+#export outdir=eboss_elg
 #export outdir=elg_dr5_1000per
-#export outdir=elg_dr5_500per
-export thedate="02-27-2018"
+export outdir=elg_dr5_500per
+#export thedate="02-27-2018"
+export thedate="03-05-2018"
 export derived_dir="${CSCRATCH}/obiwan_out/${outdir}/derived_${thedate}"
-export bricks_fn=${CSCRATCH}/obiwan_out/${outdir}/bricks.txt
+export bricks_fn=${derived_dir}/bricks.txt
 #export bricks_fn=${CSCRATCH}/obiwan_out/bricks100.txt
 
 # Load production env
@@ -34,9 +35,9 @@ else
 fi
 
 let tasks=${num_cores}*${SLURM_JOB_NUM_NODES}
-srun -n 1 -c 1 \
+srun -n 1 -c ${num_cores} \
     python -u $CSCRATCH/obiwan_code/obiwan/py/obiwan/runmanager/merge_tables.py \
     --doWhat ${doWhat} --derived_dir ${derived_dir} \
     --bricks_fn ${bricks_fn} --nproc ${tasks} \
-    --merge_rank_tables 
+    --merge_rank_tables --randoms_subset 
 
